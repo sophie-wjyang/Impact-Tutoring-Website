24ae6c68004614ab7291e32010de4f496c2c1298
Sophie Yang <sophieyang@Sophies-MacBook-Pro.local>
2023-11-21 22:22:46 -0800

    initialized components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import WhoWeAre from "./components/WhoWeAre";
+import WhatWeDo from "./components/WhatWeDo";
+import HowItWorks from "./components/HowItWorks";
+import CallToAction from "./components/CallToAction";
+
 function App() {
-  return null
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <WhoWeAre />
+      <WhatWeDo />
+      <HowItWorks />
+      <CallToAction />
+    </>
+  )
 }
 
 export default App;
