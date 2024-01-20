09e60268631aeec2e5118d1dc0c53d9563142be3
Sophie Yang <sophieyang@Sophies-MacBook-Pro.local>
2024-01-20 18:43:35 -0500

    database initial setup

---
+++ backend/sql/create_tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE tutors (
+    id serial PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    grade integer,
+    gender varchar(255),
+    location varchar(255),
+    subjects TEXT[],
+    languages TEXT[],
+    availability TEXT[],
+    student_capacity integer,
+    report_card BYTEA,
+    resume BYTEA,
+    previous_experience TEXT
+);
+
+CREATE TABLE tutees (
+    id serial PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    grade integer,
+    gender varchar(255),
+    location varchar(255),
+    subjects TEXT[],
+    languages TEXT[],
+    availability TEXT[],
+    additional_information TEXT
+);
