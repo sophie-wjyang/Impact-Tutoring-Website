801b5598e20f8c8d3b459d2eea88a390ceb29dae
Sophie Yang <sophieyang@Sophies-MacBook-Pro-2.local>
2024-02-22 12:22:51 -0500

    styling for personal info table

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import DashboardSidebar from "../components/DashboardSidebar";
 
 // bootstrap
-import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 
 export default function ProfilePage() {
+    // get all relevant profile information on component render
+    useEffect(() => {
+        axios.get("http://localhost:5000/get-profile-info").then((res) => {
+            // const profileInfo = res.data;
+            console.log(res.data);
+
+            // Accessing fields from the first profile
+            // console.log('First name:', profileInfo[0].first_name);
+            // console.log('Last name:', profileInfo[0].last_name);
+            // console.log('Email:', profileInfo[0].email);
+            // console.log('Grade:', profileInfo[0].grade);
+        });
+    }, []);
+
     return (
         <Container fluid>
             <Row>
@@ -15,7 +30,40 @@ export default function ProfilePage() {
                     <DashboardSidebar />
                 </Col>
                 <Col id="dashboard-main-content" xs={9}>
-                    <h1>Welcome, {}</h1>
+                    <h1 className="profile-heading">Welcome, Sophie</h1>
+
+                    <Container className="table-container">
+                        <Table className="profile-table">
+                            <thead>
+                                <tr>
+                                    <th className="personal-information">Personal information</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="personal-information-header"><b>Name</b></td>
+                                    <td className="personal-information-text">Sophie Yang</td>
+                                </tr>
+                                <tr>
+                                    <td className="personal-information-header"><b>Email</b></td>
+                                    <td className="personal-information-text">sophie.wjyang@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <td className="personal-information-header"><b>Grade</b></td>
+                                    <td className="personal-information-text">12</td>
+                                </tr>
+                                <tr>
+                                    <td className="personal-information-header"><b>Gender</b></td>
+                                    <td className="personal-information-text">Female</td>
+                                </tr>
+                                <tr>
+                                    <td className="personal-information-header"><b>Location</b></td>
+                                    <td className="personal-information-text">Toronto, Canada</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Container>
                 </Col>
             </Row>
         </Container>
