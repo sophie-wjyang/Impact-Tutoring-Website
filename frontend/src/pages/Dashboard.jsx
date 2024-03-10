36ec3e2e55480ddf334fc38886e6f71f7b302d6d
Sophie Yang <sophie.wjyang@gmail.com>
2024-03-10 16:16:18 -0400

    finished making sidebar responsive

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 // fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,49 +18,89 @@ import ProfilePage from "./ProfilePage";
 import UpcomingSessionsPage from "./UpcomingSessionsPage";
 
 // bootstrap
-import { Container, Row, Col, Nav } from "react-bootstrap";
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
 
 export default function DashboardSidebar() {
     const [activeKey, setActiveKey] = useState("/profile");
+    const [flexDirection, setFlexDirection] = useState(window.innerWidth >= 1200);
+
+    // change flex direction based on window width
+    useEffect(() => {
+        const handleResize = () => {
+            setFlexDirection(window.innerWidth >= 1200);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <Container fluid>
             <Row>
                 {/* sidebar */}
                 <Col id="dashboard-sidebar" xl={3}>
-                    <Nav className="flex-column sidebar-links" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
-                        <img src={Logo} className="d-inline-block sidebar-logo" />
-                        <Nav.Link eventKey="/profile" className="sidebar-link">
-                            <FontAwesomeIcon icon={faUser} className="sidebar-icon" />
-                            Profile
-                        </Nav.Link>
-                        <Nav.Link eventKey="/upcoming-sessions" className="sidebar-link">
-                            <FontAwesomeIcon icon={faCalendarDays} className="sidebar-icon" />
-                            Upcoming sessions
-                        </Nav.Link>
-                        <Nav.Link eventKey="/my-tutees" className="sidebar-link">
-                            <FontAwesomeIcon icon={faChalkboardUser} className="sidebar-icon" />
-                            My tutees
-                        </Nav.Link>
-                        <Nav.Link eventKey="/resources" className="sidebar-link">
-                            <FontAwesomeIcon icon={faFileLines} className="sidebar-icon" />
-                            Resources
-                        </Nav.Link>
-                        <Nav.Link eventKey="/volunteer-hours" className="sidebar-link">
-                            <FontAwesomeIcon icon={faClock} className="sidebar-icon" />
-                            Volunteer hours
-                        </Nav.Link>
-                        <Nav.Link eventKey="/log-out" className="sidebar-link">
-                            <FontAwesomeIcon icon={faArrowRightFromBracket} className="sidebar-icon" />
-                            Log out
-                        </Nav.Link>
-                    </Nav>
+                    <Navbar className={`flex-${flexDirection ? "column" : "row"}`} expand="xl" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)} collapseOnSelect>
+                        <Navbar.Brand>
+                            <img src={Logo} className="d-inline-block sidebar-logo" alt="Logo" />
+                        </Navbar.Brand>
+
+                        <Navbar.Toggle aria-controls="sidebar-nav" />
+
+                        <Navbar.Collapse className="w-100">
+                            <Nav className="flex-column sidebar-links w-100" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
+                                <Nav.Link eventKey="/profile" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faUser} className="sidebar-icon" />
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link eventKey="/upcoming-sessions" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faCalendarDays} className="sidebar-icon" />
+                                    Upcoming sessions
+                                </Nav.Link>
+                                <Nav.Link eventKey="/my-tutees" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faChalkboardUser} className="sidebar-icon" />
+                                    My tutees
+                                </Nav.Link>
+                                <Nav.Link eventKey="/resources" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faFileLines} className="sidebar-icon" />
+                                    Resources
+                                </Nav.Link>
+                                <Nav.Link eventKey="/volunteer-hours" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faClock} className="sidebar-icon" />
+                                    Volunteer hours
+                                </Nav.Link>
+                                <Nav.Link eventKey="/log-out" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faArrowRightFromBracket} className="sidebar-icon" />
+                                    Log out
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
                 </Col>
+
+                {/* <Col id="dashboard-sidebar" xl={3}>
+                    <Navbar expand="xl" className="flex-column">
+                        <Navbar.Brand>
+                            <img src={Logo} className="d-inline-block sidebar-logo" alt="Logo" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="sidebar-nav" />
+                        <Navbar.Collapse id="sidebar-nav">
+                            <Nav className="flex-column sidebar-links" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
+                                <Nav.Link eventKey="/profile" className="sidebar-link">
+                                    <FontAwesomeIcon icon={faUser} className="sidebar-icon" />
+                                    Profile
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Col> */}
 
                 {/* main content */}
                 <Col id="dashboard-main-content" xl={9}>
-                    {activeKey === "/profile" && <ProfilePage /> }
-                    {activeKey === "/upcoming-sessions" && <UpcomingSessionsPage /> }
+                    {activeKey === "/profile" && <ProfilePage />}
+                    {activeKey === "/upcoming-sessions" && <UpcomingSessionsPage />}
                 </Col>
             </Row>
         </Container>
