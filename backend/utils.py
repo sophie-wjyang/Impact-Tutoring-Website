328010880351e6c92206f41c420a2d794ef21faa
Alex Yu <alex@alexjy.com>
2024-08-03 17:38:15 -0400

    signup confirmation email

--- backend/utils.py
+++ backend/utils.py
@@ -1,14 +1,32 @@
+import os
+import uuid
+
 from flask_mail import Message
 
-# def generateEmailConfirmationCode():
+from app import mail
 
 
-# def sendEmailConfirmationCode(
-#     email,
-# ):
-#     msg = Message(
-#         subject="Impact Tutoring: Confirm your email",
-#         sender=("Impact Tutoring", "impacttutoringca@gmail.com"),
-#         recipients=[email],
-#         html=
-#     )
+def create_email_confirmation_code(cur, user_id, email):
+    code = uuid.uuid4().hex
+
+    cur.execute(
+        """INSERT INTO confirmation_codes (user_id, email, code) VALUES (%s, %s, %s)""",
+        (user_id, email, code),
+    )
+
+    return code
+
+
+def send_confirmation_email(
+    cur,
+    user_id,
+    email,
+):
+    code = create_email_confirmation_code(cur, user_id, email)
+
+    mail.send(Message(
+        subject="Impact Tutoring: Confirm your email",
+        sender=("Impact Tutoring", "impacttutoringca@gmail.com"),
+        recipients=[email],
+        body=f"Please click the following link to confirm your email: {os.environ["CLIENT_URL"]}/confirm_email?code={code}",
+    ))
